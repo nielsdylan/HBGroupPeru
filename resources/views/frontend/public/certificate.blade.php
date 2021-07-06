2734148f11671b87c1f5fb3fed33ada60dbec956
nielsdylan <niels_dylan@hotmail.com>
2021-07-06 08:37:51 -0500

    cambios del diseño de los certificados listado

--- resources/views/frontend/public/certificate.blade.php
+++ resources/views/frontend/public/certificate.blade.php
@@ -4,17 +4,44 @@
     <section id="certificate-section">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 text-center">
+                <div class="col-md-12 text-center animated slideInUp">
                     <h4>Certificados</h4>
                 </div>
             </div>
-            <div class="row">
+            <div class="row d-none d-sm-none d-lg-block d-md-block animated slideInUp">
                 <div class="col-md-6 offset-3  text-justify">
                     <p>La empresa HB GROUP PERÚ S.R.L. brindar servicios de calidad, pone a disposición, el siguiente formulario en el cual pueden descargar los certificados correspondientes.</p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-6 offset-3">
+            <div class="row d-block d-sm-block d-lg-none d-md-none animated slideInUp">
+                <div class="col-md-12 text-justify">
+                    <p>La empresa HB GROUP PERÚ S.R.L. brindar servicios de calidad, pone a disposición, el siguiente formulario en el cual pueden descargar los certificados correspondientes.</p>
+                </div>
+            </div>
+            <div class="row d-none d-sm-none d-lg-block d-md-block ">
+                <div class="col-md-6 offset-3 animated slideInUp">
+                    <div class="card">
+                        <div class="card-body">
+                            <form action="{{route('certificate.list')}}" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="dni">Ingrese su número de documento:</label>
+                                            <input id="dni" class="form-control" type="text" name="dni">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12 text-center">
+                                        <button type="submit" class="btn btn-primary"><i class="fas fa-certificate"></i> Verificar certificado</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row d-block d-sm-block d-lg-none d-md-none">
+                <div class="col-md-12 animated slideInUp">
                     <div class="card">
                         <div class="card-body">
                             <form action="{{route('certificate.list')}}" method="POST">
@@ -36,8 +63,8 @@
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-md-6 offset-3 mt-5">
+            <div class="row d-none d-sm-none d-lg-block d-md-block">
+                <div class="col-md-6 offset-3 mt-5 animated slideInUp">
                     @if ($certificado)
                     <div class="card">
                         <div class="card-body">
@@ -52,7 +79,50 @@
                             @else
                             <h5 class="card-title">Listado de certificados</h5>
                             <div class="row">
+                                <div class="col-md-12 table-responsive">
+                                    <table class="table">
+                                        <tbody>
+                                            @foreach ($certificado as $certi)
+                                                <tr>
+                                                    <td>{{ $certi->last_name.' '.$certi->name }}</td>
+                                                    <td>{{ $certi->description_cours }}</td>
+                                                    <td>{{ date('d/m/Y', strtotime($certi->date)) }}</td>
+                                                    <td><a href="{{route('certificado.pdf',$certi->certificado_id )}}"><i class="fas fa-cloud-download-alt"></i> PDF</a></td>
+                                                </tr>
+
+                                            @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 d-flex justify-content-center">
+                                    {{ $certificado->links() }}
+                                </div>
+                            </div>
+                            @endif
+                        </div>
+                    </div>
+                    @endif
+                </div>
+            </div>
+            <div class="row d-block d-sm-block d-lg-none d-md-none">
+                <div class="col-md-12 mt-5 animated slideInUp">
+                    @if ($certificado)
+                    <div class="card">
+                        <div class="card-body">
+                            @if ($message)
+                            <div class="row">
                                 <div class="col-md-12">
+                                    <div class="alert alert-warning" role="alert">
+                                        {{$message}}
+                                    </div>
+                                </div>
+                            </div>
+                            @else
+                            <h5 class="card-title">Listado de certificados</h5>
+                            <div class="row">
+                                <div class="col-md-12 table-responsive">
                                     <table class="table">
                                         <tbody>
                                             @foreach ($certificado as $certi)
