87269d6210415cf3b2729509de8344dd80967e24
nielsdylan <niels_dylan@hotmail.com>
2023-07-13 00:35:58 -0500

    cambios de public

--- public_old/assets/js/demo.js
+++ public_old/assets/js/demo.js
@@ -1,19 +1,19 @@
 "use strict";
 // Cicle Chart
-Circles.create({
-	id:           'task-complete',
-	radius:       50,
-	value:        80,
-	maxValue:     100,
-	width:        5,
-	text:         function(value){return value + '%';},
-	colors:       ['#36a3f7', '#fff'],
-	duration:     400,
-	wrpClass:     'circles-wrp',
-	textClass:    'circles-text',
-	styleWrapper: true,
-	styleText:    true
-})
+// Circles.create({
+// 	id:           'task-complete',
+// 	radius:       50,
+// 	value:        80,
+// 	maxValue:     100,
+// 	width:        5,
+// 	text:         function(value){return value + '%';},
+// 	colors:       ['#36a3f7', '#fff'],
+// 	duration:     400,
+// 	wrpClass:     'circles-wrp',
+// 	textClass:    'circles-text',
+// 	styleWrapper: true,
+// 	styleText:    true
+// })
 
 //Notify
 // $.notify({
@@ -30,29 +30,29 @@ Circles.create({
 // });
 
 // JQVmap
-$('#map-example').vectorMap(
-{
-	map: 'world_en',
-	backgroundColor: 'transparent',
-	borderColor: '#fff',
-	borderWidth: 2,
-	color: '#e4e4e4',
-	enableZoom: true,
-	hoverColor: '#35cd3a',
-	hoverOpacity: null,
-	normalizeFunction: 'linear',
-	scaleColors: ['#b6d6ff', '#005ace'],
-	selectedColor: '#35cd3a',
-	selectedRegions: ['ID', 'RU', 'US', 'AU', 'CN', 'BR'],
-	showTooltip: true,
-	onRegionClick: function(element, code, region)
-	{
-		return false;
-	},
-	onResize: function (element, width, height) {
-		console.log('Map Size: ' +  width + 'x' +  height);
-	},
-});
+// $('#map-example').vectorMap(
+// {
+// 	map: 'world_en',
+// 	backgroundColor: 'transparent',
+// 	borderColor: '#fff',
+// 	borderWidth: 2,
+// 	color: '#e4e4e4',
+// 	enableZoom: true,
+// 	hoverColor: '#35cd3a',
+// 	hoverOpacity: null,
+// 	normalizeFunction: 'linear',
+// 	scaleColors: ['#b6d6ff', '#005ace'],
+// 	selectedColor: '#35cd3a',
+// 	selectedRegions: ['ID', 'RU', 'US', 'AU', 'CN', 'BR'],
+// 	showTooltip: true,
+// 	onRegionClick: function(element, code, region)
+// 	{
+// 		return false;
+// 	},
+// 	onResize: function (element, width, height) {
+// 		console.log('Map Size: ' +  width + 'x' +  height);
+// 	},
+// });
 
 //Chart
 
